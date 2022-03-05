012e90c563c61d1689eb4a105c197499e8c0eb72
DemetriusADS <demetriusegundo@gmail.com>
2022-03-05 18:09:36 -0300

    feat: add custom erros in login controller

--- src/controllers/login.controller.ts
+++ src/controllers/login.controller.ts
@@ -1,20 +1,43 @@
-import { ControllerPort, ForValidateEmailPort, HttpPort } from '@src/ports'
+import {
+  ControllerPort,
+  ForValidateEmailPort,
+  GenerateEncryptedCodePort,
+  HttpPort
+} from '@src/ports'
 import { LoginRequestDTO, LoginResponseDTO } from '@src/dto'
-import { InvalidParamError } from '../errors'
+import { InvalidParamError, MissingParamError } from '../errors'
 
 export class LoginController implements ControllerPort {
-  constructor(private readonly isValidEmail: ForValidateEmailPort) {}
+  constructor(
+    private readonly isValidEmail: ForValidateEmailPort,
+    private readonly generateEncryptedCodeCryptoAdapter: GenerateEncryptedCodePort
+  ) {}
+
   async execute({
     data
   }: HttpPort.Params<LoginRequestDTO>): Promise<HttpPort.Result<LoginResponseDTO>> {
+    const requiredParams = ['email', 'password']
+    for (const requiredParam of requiredParams) {
+      if (!data[requiredParam]) {
+        throw new MissingParamError(requiredParam)
+      }
+    }
     if (!this.isValidEmail.execute(data.email)) {
       throw new InvalidParamError('email')
     }
 
+    const token = this.generateEncryptedCodeCryptoAdapter.execute({
+      toEncrypt: {
+        email: data.email,
+        password: data.password
+      },
+      lifeTime: 5000
+    })
+
     return await Promise.resolve({
       statusCode: 200,
       responseBody: {
-        token: ''
+        token
       }
     })
   }
