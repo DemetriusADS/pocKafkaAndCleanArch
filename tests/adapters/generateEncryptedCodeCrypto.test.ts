47a4f148de2e6a9c4dbcfeb5a5403dd9cf3e1d0f
DemetriusADS <demetriusegundo@gmail.com>
2022-03-06 16:17:59 -0300

    style: remove line

--- tests/adapters/generateEncryptedCodeCrypto.test.ts
+++ tests/adapters/generateEncryptedCodeCrypto.test.ts
@@ -12,7 +12,6 @@ const makeSut = (): Sut => {
 describe('Generate Encrypted Code using Crypto', () => {
   test('should return a hmacSh256 hash', () => {
     const { sut, fixture } = makeSut()
-
     const testable = sut.execute(fixture)
     expect(testable).toEqual('wgWbNVctIvdtx1WZj5jUSBNmZgydViGlm+PaUjf8cY4=')
   })
