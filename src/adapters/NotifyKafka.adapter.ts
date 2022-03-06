ca27454d3cc1aab1b89e64ae55ca8cd3d0d89513
DemetriusADS <demetriusegundo@gmail.com>
2022-03-06 18:17:15 -0300

    feat: add notify kafka adapter

---
+++ src/adapters/NotifyKafka.adapter.ts
@@ -0,0 +1,17 @@
+import { NotifyTopicPort } from '@src/ports'
+import { Kafka } from 'kafkajs'
+
+export class NotifyKafkaAdapter implements NotifyTopicPort<any> {
+  constructor(private readonly kafka: Kafka) {}
+
+  async send({ topic, message }: NotifyTopicPort.Params<any>) {
+    const producer = this.kafka.producer()
+    await producer.connect()
+    const toBeSended = {
+      topic,
+      messages: [{ ...message }]
+    }
+    await producer.send(toBeSended)
+    await producer.disconnect()
+  }
+}
--- src/adapters/index.ts
+++ src/adapters/index.ts
@@ -1,1 +1,2 @@
 export * from './generateEncryptedCodeCrypto.adapter'
+export * from './NotifyKafka.adapter'
